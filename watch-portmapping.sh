87a4a6f57a4ad663989c52949f0c8584396b665d
Pieter Lange <pieter@ptlc.nl>
2016-12-17 14:15:31 +0100

    Use separate iptables chain for portforwards

--- watch-portmapping.sh
+++ watch-portmapping.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 [ $DEBUG ] && set -x
 
+iptables -t nat -N KUBEOPENVPNPORTFORWARD
+iptables -t nat -A PREROUTING -j KUBEOPENVPNPORTFORWARD
+
 while true; do
     if [ -d $OVPN_CCD ]; then
 
         if [ -d $OVPN_PORTMAPPING ]; then
             # Flush any old NAT rules.
-            iptables -t nat -F
+            iptables -t nat -F KUBEOPENVPNPORTFORWARD
+
             for port in $(ls -1 ${OVPN_PORTMAPPING}); do
                 dest_cname=$(cut -d':' -f1 ${OVPN_PORTMAPPING}/${port})
                 dest_port=$(cut -d':' -f2 ${OVPN_PORTMAPPING}/${port})
                 if [ -f ${OVPN_CCD}/${dest_cname} ]; then
                     dest_ip=$(grep 'ifconfig-push' $OVPN_CCD/${dest_cname} | cut -d' ' -f2)
                     echo "Routing ${PODIPADDR}:${port} to ${dest_ip}:${dest_port} (${dest_cname})"
-                    iptables -t nat -A PREROUTING -p tcp -d $PODIPADDR --dport ${port} -j DNAT --to ${dest_ip}:${dest_port}
+                    iptables -t nat -A KUBEOPENVPNPORTFORWARD -p tcp -d $PODIPADDR --dport ${port} -j DNAT --to ${dest_ip}:${dest_port}
                 else
                     echo "ERROR: client configuration for ${dest_cname} not found"
                 fi
